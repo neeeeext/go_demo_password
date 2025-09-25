2369b55dd583c87a2ea0a36bcba665c50e605b7e
Andrew <andrei.zaitcev81@gmail.com>
2025-09-25 18:45:09 +0300

    runes

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 )
+
+var letter = []rune("abcdefghijklmnopqrstuvwxyz")
 
 type account struct {
 	Login    string
@@ -11,6 +14,18 @@ type account struct {
 }
 
 func main() {
+	fmt.Println(len(letter))
+
+	str := []rune("Привет!)")
+	for _, ch := range string(str) {
+		fmt.Println(ch, string(ch))
+	}
+	fmt.Println("Введите сколько знаков в пароле хотите")
+	var rndNumber int
+	fmt.Scan(&rndNumber)
+	rndPassword := generatePassword(rndNumber)
+	fmt.Println(rndPassword)
+
 	for {
 		login := printData("Введите свой логин")
 		password := printData("Введите свой пароль")
@@ -38,4 +53,14 @@ func outputAccount(a *account) {
 	fmt.Println(a.Login)
 	fmt.Println(a.Password)
 	fmt.Println(a.Url)
+}
+
+func generatePassword(n int) string {
+	password := make([]rune, n)
+
+	for i := 0; i < n; i++ {
+		password[i] = letter[rand.IntN(len(letter))]
+
+	}
+	return string(password)
 }
