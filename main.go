a6c550e587e689673828ca962510a72687fdcf99
Andrew <andrei.zaitcev81@gmail.com>
2025-09-25 23:47:57 +0300

    update 1.0

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
-var letter = []rune("abcdefghijklmnopqrstuvwxyz")
+// const
+var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$")
+
+//Struct
 
 type account struct {
 	Login    string
@@ -13,54 +18,83 @@ type account struct {
 	Url      string
 }
 
-func main() {
-	fmt.Println(len(letter))
+func newAccount(login, password, urlString string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_URL")
 
-	str := []rune("Привет!)")
-	for _, ch := range string(str) {
-		fmt.Println(ch, string(ch))
 	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	newAccount := &account{
+		Login:    login,
+		Password: password,
+		Url:      urlString,
+	}
+	if password == "" {
+		newAccount.generatePassword()
+	}
+	return newAccount, nil
+}
+
+//Method
+
+func (a *account) outputAccount() {
+	fmt.Println(a.Login, a.Password, a.Url)
+}
+
+func (a *account) generatePassword() {
+
 	fmt.Println("Введите сколько знаков в пароле хотите")
 	var rndNumber int
 	fmt.Scan(&rndNumber)
-	rndPassword := generatePassword(rndNumber)
-	fmt.Println(rndPassword)
 
-	for {
-		login := printData("Введите свой логин")
-		password := printData("Введите свой пароль")
-		url := printData("Введите свой url")
+	password := make([]rune, rndNumber)
 
-		account := account{
-			Login:    login,
-			Password: password,
-			Url:      url,
-		}
-		outputAccount(&account)
+	for i := 0; i < rndNumber; i++ {
+		password[i] = letter[rand.IntN(len(letter))]
 
 	}
 
+	a.Password = string(password)
+	fmt.Println("Ваш пароль сгенирован и записан")
 }
+
+//Main
+
+// 1. Если логина нет, то ошибка
+// 2. Если нет пароля, то генерим
+func main() {
+
+	for {
+
+		login := printData("Введите свой логин")
+
+		var password string
+		fmt.Println("Введите свой пароль")
+		fmt.Scanln(&password)
+
+		url := printData("Введите свой url")
+
+		account, err := newAccount(login, password, url)
+
+		if err != nil {
+			fmt.Println("Неверный формат URL или login!")
+			return
+		}
+
+		account.outputAccount()
+
+	}
+}
+
+//Function
 
 func printData(promt string) string {
 	fmt.Println(promt)
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
-}
-
-func outputAccount(a *account) {
-	fmt.Println(a.Login)
-	fmt.Println(a.Password)
-	fmt.Println(a.Url)
-}
-
-func generatePassword(n int) string {
-	password := make([]rune, n)
-
-	for i := 0; i < n; i++ {
-		password[i] = letter[rand.IntN(len(letter))]
-
-	}
-	return string(password)
 }
