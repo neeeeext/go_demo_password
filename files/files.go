5619d7edc6d2799a0237ec8349243a389ce275dc
Andrew <andrei.zaitcev81@gmail.com>
2025-09-26 01:20:07 +0300

    add os and defer

---
+++ files/files.go
@@ -0,0 +1,25 @@
+package files
+
+import (
+	"fmt"
+	"os"
+)
+
+func WriteFile(content string, name string) {
+	file, err := os.Create(name)
+	if err != nil {
+		fmt.Println("Ошибка создания файла")
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Запись успешна")
+}
+
+func ReadFile() {
+
+}
--- main.go
+++ main.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"app/account"
+	"app/files"
 	"fmt"
 )
 
 func main() {
 
-	for {
+	files.WriteFile("Одаб", "dabdab.txt")
+	login := printData("Введите свой логин")
 
-		login := printData("Введите свой логин")
+	var password string
+	fmt.Println("Введите свой пароль")
+	fmt.Scanln(&password)
 
-		var password string
-		fmt.Println("Введите свой пароль")
-		fmt.Scanln(&password)
+	url := printData("Введите свой url")
 
-		url := printData("Введите свой url")
+	account, err := account.NewAccount(login, password, url)
 
-		account, err := account.NewAccount(login, password, url)
-
-		if err != nil {
-			fmt.Println("Неверный формат URL или login!")
-			return
-		}
-
-		account.OutputAccount()
-
+	if err != nil {
+		fmt.Println("Неверный формат URL или login!")
+		return
 	}
+
+	account.OutputAccount()
+	files.ReadFile()
+
 }
 
 func printData(promt string) string {
