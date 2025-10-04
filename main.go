5921b2c38d8f7fa463e16c9b4ea5231220990f39
Andrew <andrei.zaitcev81@gmail.com>
2025-10-04 17:55:48 +0300

    `Refactor main function to use a menu map for variant handling`

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/fatih/color"
 )
 
+var menu = map[string]func(*account.VaultWithDb){
+	"1": createAccount,
+	"2": findAccount,
+	"3": deleteAccount,
+}
+
 func main() {
 	vault := account.NewVault(files.NewJsonDb("data.json"))
 Menu:
@@ -21,16 +27,22 @@ Menu:
 			"Выберите что вы хотите сделать: ",
 		})
 
-		switch variant {
-		case "1":
-			createAccount(vault)
-		case "2":
-			findAccount(vault)
-		case "3":
-			deleteAccount(vault)
-		default:
+		menuFunc := menu[variant]
+		if menuFunc == nil {
 			break Menu
 		}
+		menuFunc(vault)
+
+		// switch variant {
+		// case "1":
+		// 	createAccount(vault)
+		// case "2":
+		// 	findAccount(vault)
+		// case "3":
+		// 	deleteAccount(vault)
+		// default:
+		// 	break Menu
+		// }
 	}
 }
 func findAccount(vault *account.VaultWithDb) {
